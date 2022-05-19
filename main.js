4b0af754a4d143a0076602e855211f2c740aa291
sara-likaount <saralikaunt@gmail.com>
2022-05-19 18:34:07 +0300

    b

---
+++ main.js
@@ -0,0 +1,19 @@
+var somename="";
+function nameinstring(somename){// "sara likaount"
+    for(i=0; i< somename.leangth; i++){
+        var missingchar=" "; 
+        if(){
+            
+            console.log();
+        }
+        else if(){
+            var stringarry= somename;
+            return stringarry;
+            
+        }
+        else if(){
+           return somename.capital(); 
+        }
+    }
+
+}
