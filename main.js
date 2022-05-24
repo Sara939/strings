c2ee957d17f39c7e91be361a1d86b5c75cc76e33
sara-likaount <saralikaunt@gmail.com>
2022-05-25 00:11:32 +0300

    M

--- main.js
+++ main.js
@@ -1,19 +1,82 @@
-var somename="";
-function nameinstring(somename){// "sara likaount"
-    for(i=0; i< somename.leangth; i++){
-        var missingchar=" "; 
-        if(){
+// var somename="";
+// function nameinstring(somename){// "sara likaount"
+//     for(i=0; i< somename.leangth; i++){
+//         var missingchar=" "; 
+//         if(){
             
-            console.log();
-        }
-        else if(){
-            var stringarry= somename;
-            return stringarry;
+//             console.log();
+//         }
+//         else if(){
+//             var stringarry= somename;
+//             return stringarry;
             
+//         }
+//         else if(){
+//            return somename.capital(); 
+//         }
+//     }
+
+// 112:
+// function newline(somestring){
+//     for(i=0;i<somestring.length; i++){
+//         console.log(somestring[i]);
+//     }
+    
+// }
+// newline("string");
+
+// 113:
+
+// function bildnew(sentence){
+//     newstring="";
+//     for(i=0; i<sentence.length; i++){
+//         newstring+=sentence[i]+i;
+//     }
+//     console.log(newstring);
+// }
+// bildnew("sara");
+
+// 114:
+// function replacetwo(a){
+//     var my= "";
+//     for(i=0; i<a.length; i++){
+//         if(i % 2==0){
+//             my+=2;
+//         }
+//         else{
+//             my+=a[i]
+//         }
+//     }
+//     return my;
+// }
+// console.log(replacetwo("string"));
+
+// 115:
+// function firstname(name){
+//     for(i=0; i<firstname.length; i++){
+//         if(name[0]=="A" || name[0]=="a"){
+//             console.log("hello");
+//         }
+//         else{
+//             console.log("bye")
+//         }
+//     }
+// }
+
+// firstname("al");
+
+// 116:
+function lastname(name){
+    for(i=0; i<name.length; i++){
+        if((name.length)-1[i]=="E"){
+            console.log("yes");
         }
-        else if(){
-           return somename.capital(); 
+        else{
+            console.log("no")
         }
     }
-
 }
+
+lastname("E");
+
+
